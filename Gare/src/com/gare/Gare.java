27e1ac5126833ffef21c596cf17a0536892728c4
Tommaso <tommaso.giacomelli@polomanettiporciatti.edu.it>
2020-11-18 21:58:28 +0100

    Aggiunto testo esercizio

---
+++ Gare/src/com/gare/Gare.java
@@ -0,0 +1,13 @@
+package com.gare;
+
+public class Gare {
+    // TODO Classe
+    // Creare una classe gare con i seguenti attributi:
+    //- nome gara (String) ---> (esempio: maratona di New York)
+    //- tipo sport (String) ---> (esempio: corsa)
+    //- premio in denaro (double) ---> (esempio: 5000€)
+    //- nome vincitore (String) ---> (esempio: Mario Rossi)
+    //
+    //Creare il metodo csotruttore (ovviamente nel metodo costruttore il nome del vinciore non sarà messo)
+    // e i get e set.
+}
